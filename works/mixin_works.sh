3f0980ca54d975d3f42004baabd121c8412b431d
Robin Wen <blockxyz@gmail.com>
2021-03-15 07:55:34 +0800

    Update memo

--- works/mixin_works.sh
+++ works/mixin_works.sh
@@ -23,20 +23,22 @@ access_token="$(config_get ACCESS_TOKEN)"
 
 leader_works_first=`$mixin -n $host getinfo | jq '.' | grep ${node_id} -A 10 | grep works -A 2 | grep -v works | sed "s/,//g" | sed -n '1p' | sed "s/ //g"`
 signer_works_first=`$mixin -n $host getinfo | jq '.' | grep ${node_id} -A 10 | grep works -A 2 | grep -v works | sed "s/,//g" | sed -n '2p' | sed "s/ //g"`
-log="`date '+%Y-%m-%d %H:%M:%S'` UTC `hostname` `whoami` INFO leader_works: ${leader_works_first}, signer_works: ${signer_works_first}"
+works_first=$((leader_works_first + signer_works_first))
+log="`date '+%Y-%m-%d %H:%M:%S'` UTC `hostname` `whoami` INFO leader_works: ${leader_works_first}, signer_works: ${signer_works_first}, works_first: ${works_first}"
 echo $log >> $log_file
 
 sleep 600
 
 leader_works_second=`$mixin -n $host getinfo | jq '.' | grep ${node_id} -A 10 | grep works -A 2 | grep -v works | sed "s/,//g" | sed -n '1p' | sed "s/ //g"`
 signer_works_second=`$mixin -n $host getinfo | jq '.' | grep ${node_id} -A 10 | grep works -A 2 | grep -v works | sed "s/,//g" | sed -n '2p' | sed "s/ //g"`
-log="`date '+%Y-%m-%d %H:%M:%S'` UTC `hostname` `whoami` INFO leader_works: ${leader_works_second}, signer_works: ${signer_works_second}"
+works_second=$((leader_works_second + signer_works_second))
+log="`date '+%Y-%m-%d %H:%M:%S'` UTC `hostname` `whoami` INFO leader_works: ${leader_works_second}, signer_works: ${signer_works_second}, works_second: ${works_second}"
 echo $log >> $log_file
 
-if [ ${leader_works_first} -eq ${leader_works_second} ] || [ ${signer_works_first} -eq ${signer_works_second} ]
+if [ ${works_first} -eq ${works_second} ]
 then
-    log="`date '+%Y-%m-%d %H:%M:%S'` UTC `hostname` `whoami` ERROR ${service} $host status is abnormal, leader works or signer works for more than 10 minutes without change."
-    echo $log >> $log_file
+    log="时间: `date '+%Y-%m-%d %H:%M:%S'` UTC \n 主机名: `hostname` \n 节点: $host \n 第一次 works 累计: ${works_first} \n 第二次 works 累计: ${works_second} \n 状态: 10 分钟没变化，已重启节点"
+    echo -e $log >> $log_file
     success=`curl ${webhook_url}=${access_token} -XPOST -H 'Content-Type: application/json' -d '{"category":"PLAIN_TEXT","data":"'"$log"'"}' | awk -F',' '{print $1}' | awk -F':' '{print $2}'`
     if [ "$success" = "true" ]
     then
